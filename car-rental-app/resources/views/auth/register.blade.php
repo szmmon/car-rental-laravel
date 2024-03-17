30e20c4188ccd0a7a749fdf76e3804477b79ab10
Szymon Ziemianek <szymonziemianek@gmail.com>
2024-03-17 12:58:48 +0100

    #9 added mail verification, testing via mailtrap

--- car-rental-app/resources/views/auth/register.blade.php
+++ car-rental-app/resources/views/auth/register.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container register-login">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-mt-4">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
--- car-rental-app/routes/web.php
+++ car-rental-app/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BookRequestController;
 use App\Http\Controllers\CarController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 
 /*
@@ -23,18 +25,32 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link resent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('/bookRequest', [BookRequestController::class, 'store'])->name('bookRequest.store');
 Route::get('/bookRequest', [BookRequestController::class, 'index'])->name('bookRequest.index');
 
-// Route::middleware(['auth'])->group(function(){
-Route::middleware(['can:isAdmin'])->group(function(){
-    Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
-    Route::get('/cars/create', [CarController::class, 'create'])->name('cars.create');
-    Route::get('/cars/edit/{car}', [CarController::class, 'edit'])->name('cars.edit');
-    Route::get('/cars/show/{car}', [CarController::class, 'show'])->name('cars.show');
-    Route::post('/cars/create', [CarController::class, 'store'])->name('cars.store');
-    Route::post('/cars/{car}', [CarController::class, 'update'])->name('cars.update');
-    Route::delete('/cars/{car}', [CarController::class, 'destroy'])->name('cars.delete');
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::middleware(['can:isAdmin'])->group(function(){
+        Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+        Route::get('/cars/create', [CarController::class, 'create'])->name('cars.create');
+        Route::get('/cars/edit/{car}', [CarController::class, 'edit'])->name('cars.edit');
+        Route::get('/cars/show/{car}', [CarController::class, 'show'])->name('cars.show');
+        Route::post('/cars/create', [CarController::class, 'store'])->name('cars.store');
+        Route::post('/cars/{car}', [CarController::class, 'update'])->name('cars.update');
+        Route::delete('/cars/{car}', [CarController::class, 'destroy'])->name('cars.delete');
+    });
 });
-// });
